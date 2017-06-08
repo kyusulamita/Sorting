a8ebc18066964b2d08451ac95b57a36ba83e0e1a
kyusulamita <sulamitamorales@yahoo.com>
2017-06-08 12:42:39 -0400

    completed bubbleSort

--- bubblesort.js
+++ bubblesort.js
@@ -1,5 +1,13 @@
 function bubbleSort(array){
 	if (array.length < 2) return array;
-
-
+	for(var i = array.length-1; i >= 0; i--){
+		for(var j = 1; j<=i; j++){//the stop
+			if(array[j-1] > array[j]){
+				var temp = array[j-1];
+				array[j-1] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+	return array;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -6,6 +6,12 @@ describe('Bubble Sort', function(){
   	expect(bubbleSort([4])).toEqual([4]);
   });
   it('handles an array of length 2', function(){
-  	expect(bubbleSort([12, 2]).toEqual([2, 12]));
+  	expect(bubbleSort([12, 2])).toEqual([2, 12]);
   });
+  it('handles an array of length 3'), function(){
+  	expect(bubbleSort([5,4,3, 2, 1])).toEqual([1, 2, 3, 4,5]);
+  }
+  it('handles arrays'), function(){
+  	expect(bubbleSort([45,12,31,2,16])).toEqual([2,12,16, 31, 45]);
+  }
 });
